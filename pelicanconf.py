3317747b5ef7ff8abc1fc49ac68078b74975a9ff
Raymundo Cassani <raymundo.cassani@gmail.com>
2019-12-28 15:24:04 -0500

    clean up on pelicanconf.py to use kis theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,197 +3,156 @@
 from __future__ import unicode_literals
 
 # To deplot locally:
-# 1. $ fab build
-# 2. $ fab serve, alternatively, $ fab reserve, does build and serve
-# 3. Open http://localhost:8000/index.html in browser
+# 1. $ ./reserve.sh
+# 2. Open http://localhost:8000/index.html in browser
+
+# To deplot online:
+# 0. Set to false RELATIVE_URLS
+# 1. $ ./prepare_html_repo.sh
+# 2. Commit and Push castoris-html
+
+###########################
+# General Pelican
+###########################
+# Language and Timezone
+TIMEZONE = 'America/Montreal'
+DEFAULT_LANG = u'en'
+DEFAULT_DATE_FORMAT = '%Y/%m/%d'
 
 # Site general information
 AUTHOR = u'Raymundo Cassani'
 SITENAME = u'CastorisCausa'
-# SITEURL = 'wwww.castoriscausa.com'
+SITEURL = 'http://www.castoriscausa.com'
 
-###########################
-# ######## THEME ######## #
-###########################
-
-# https://github.com/getpelican/pelican-themes/tree/master/pelican-bootstrap3
-THEME = ".//theme//pelican-bootstrap3"
-BOOTSTRAP_THEME = 'sandstone'  # This bootstrap3 theme serves as base
-
-###########################
-# #### EXTRAS BS3 ####### #
-###########################
-# Author info for each article
-SHOW_ARTICLE_AUTHOR = False
-
-# Category for each article
-SHOW_ARTICLE_CATEGORY = False
-
-# Modified date besides publishing date for each article
-SHOW_DATE_MODIFIED = False
-
-# Customized CSS file for CastorisCausa
-CUSTOM_CSS = 'custom_css/castoris.css'
-
-# Pygments Style, see: http://pygments.org/demo/6353539/?style=native
-PYGMENTS_STYLE = 'native'  # native, fruity, paraiso-dark
-
-# Pagination
-USE_PAGER = False
-# TRUE = shows 'Older posts' and 'Newer post' buttons
-# FALSE = shows arrows and numbers of pages <<, 1,2,3 >>
-
-# Sitelogo and size
-# SITELOGO = 'images/dr_pk.jpg'
-# SITELOGO_SIZE = 50
-
-HIDE_SITENAME = False
-
-# Breadcrumbs: HOME > Something > something
-DISPLAY_BREADCRUMBS = True
-
-# Inverse Navigation bar
-BOOTSTRAP_NAVBAR_INVERSE = False  # Leave as FALSE, and edit in castoris.css
-
-# FAVICON = 'images/dr_pk.jpg.png'
-
-DISPLAY_ARTICLE_INFO_ON_INDEX = True  # Shows Date and Tag in summaries
-
-# BANNER = 'images/dr_pk.jpg'
-# BANNER_ALL_PAGES = True
-
-# SIDEBAR_IMAGES_HEADER = 'My Images'
-SIDEBAR_IMAGES = ['../images/side_image_500.jpg']
-# Path relative to index.html in output
-
-# DISQUS
-# See this link: https://github.com/DandyDev/pelican-bootstrap3/issues/219
-DISQUSURL = 'http://castoriscausa.com'
-DISQUS_SITENAME = u"castoriscausa"
-DISQUS_NO_ID = True
-DISQUS_DISPLAY_COUNTS = True
-
-# Tag Cloud
-TAG_CLOUD_MAX_ITEMS = 10
-
-# Language and Timezone
-TIMEZONE = 'America/Montreal'
-DEFAULT_LANG = u'en'
-DATE_FORMATS = '%a, %d %b %Y'
-
-# Delete Output files everytime SSG runs
+# Delete Output files everytime Pelican runs
 DELETE_OUTPUT_DIRECTORY = True
 # Keep these files from deleting output
-OUTPUT_RETENTION = ['.git', 'CNAME']
+OUTPUT_RETENTION = ['.git']
 
-# Makes style improvements in fonts
-TYPOGRIFY = True
-CC_LICENSE = "CC-BY-NC-SA"
-
-############################
-# ####### CONTENT ######## #
-# Defines the Paths for different elements of content
-############################
-# Content root directory
-PATH = 'content'
-# Page directories
-PAGE_PATHS = ['pages']
-# Article directories
-ARTICLE_PATHS = ['articles']
-# Ignored files
-IGNORE_FILES = ['drafts', 'main.md', 'software.md']
-# Directories to be copied to OUTPUT (paths relative to Content)
-STATIC_PATHS = ['images', 'custom_css']
-# Path relative to content
-EXTRA_PATH_METADATA = {
-     './custom_css/castoris.css': {'path': 'castoris.css'}
-}
-# Plugins directory
-PLUGIN_PATHS = ['plugins/']
-PLUGINS = ['tipue_search', 'i18n_subsites']
-JINJA_ENVIRONMENT = {
-    'extensions': ['jinja2.ext.i18n'],
-}
-#DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
-DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives')
-
-
-
-############################
-# ####### OUTPUT ######### #
-# Defines paths and filenames for Output files
-############################
-# Output directory
-OUTPUT_PATH = 'output/'
-# HTML files for Pages
-PAGE_SAVE_AS = 'pages/{slug}.html'
-# HTML files for Articles
-ARTICLE_SAVE_AS = 'posts/{slug}.html'
-# HTML files for Articles with translation
-ARTICLE_LANG_SAVE_AS = 'posts/{slug}-{lang}.html'
-
-############################
-# ######## PAGES ######### #
-# Defines the URL for the elements in the website
-# www.site.com/+ ...
-############################
-# Pages
-PAGE_URL = 'pages/{slug}'
-# Articles
-ARTICLE_URL = 'posts/{slug}'
-# Articles with translation
-ARTICLE_LANG_URL = 'posts/{slug}-{lang}'
+# True if you want document-relative URLs when developing
+# False to deploy online
+RELATIVE_URLS = False
 
 ###########################
-# #### INDEX HTML LOOK ####### #
+# KIS theme
 ###########################
-# Display pages list on the top menu
-DISPLAY_PAGES_ON_MENU = True
-# Display categories list on the top menu
-DISPLAY_CATEGORIES_ON_MENU = False
-# Display categories list as a submenu of the top menu
-DISPLAY_CATEGORIES_ON_SUBMENU = True
-# Display the category in the article's info
-DISPLAY_CATEGORIES_ON_POSTINFO = False
-# Display the author in the article's info
-DISPLAY_AUTHOR_ON_POSTINFO = False
-# Display the search form
-DISPLAY_SEARCH_FORM = True
+THEME = ".//theme//pelican-kis"
 
-# Sort pages list by a given attribute
-# Add the field Sortorder to the page MD documentm e.g. Sortorder: 2
-PAGES_SORT_ATTRIBUTE = 'sortorder'
-# Measured in Words
-SUMMARY_MAX_LENGTH = 50
-# Google Analytics Track-ID
-GOOGLE_ANALYTICS = 'UA-86432394-1'
-# Number of articles per page
-DEFAULT_PAGINATION = 6
-# Order of Articles
-NEWEST_FIRST_ARCHIVES = True
+# Show elements in Menu bar
+MENUITEMS = [('HOME', '/')] # Menu items before Pages
+SHOW_PAGES_ON_MENU = True
+SHOW_CATEGORIES_ON_MENU = True
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# Subtitle string
+#TODO add check to print it
+SITESUBTITLE = u'Catchy phrase'
+
+# Show elements in Side bar
+#TODO  This should be tied to the presence of tipue_search
+SHOW_SEARCH_ON_SIDEBAR = True
+SHOW_CATEGORIES_ON_SIDEBAR = True
+SHOW_TEXT_ON_SIDEBAR = True
+SIDEBAR_TITLE_TEXT = u'Hello'
+SIDEBAR_TEXT = u'A brief text that will appear in de siderbar, this can be anything'
+SHOW_TAGS_ON_SIDEBAR = True
 
 # Blogroll
+# TODO Add this links in Sidebar
 LINKS = (
          ('Home', 'http://www.castoriscausa.com'),
          ('MuSAE Lab', 'http://musaelab.ca/')
         )
 
-# Social widget
+# Show Metadata in Indexes and Articles
+SHOW_ARTICLE_TAGS = True
+SHOW_ARTICLE_AUTHOR = False
+SHOW_ARTICLE_CATEGORY = True
+SHOW_ARTICLE_DATEMODIFIED = True
+
+# Customized CSS file for CastorisCausa
+CUSTOM_CSS = 'castoris.css'
+
+###########################
+# Others
+###########################
+# Social icons
 SOCIAL = (
           ('GitHub', 'https://github.com/rcassani'),
           ('Twitter', 'https://twitter.com/r_cassani'),
           ('LinkedIn', 'https://www.linkedin.com/in/rcassani/')
          )
 
+# DISQUS
+DISQUSURL = 'http://castoriscausa.com'
+DISQUS_SITENAME = u"castoriscausa"
+DISQUS_NO_ID = True
+DISQUS_DISPLAY_COUNTS = True
 
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+# Pygments Style, see: http://pygments.org/demo/6353539/?style=native
+PYGMENTS_STYLE = 'native'  # native, fruity, paraiso-dark
 
-# JINJA_EXTENSIONS = []
+# Google Analytics Track-ID
+GOOGLE_ANALYTICS = 'UA-86432394-1'
+
+# CC License
+CC_LICENSE = "CC-BY-NC-SA"
+
+# Plugins directory
+PLUGIN_PATHS = ['plugins/']
+PLUGINS = ['tipue_search', 'sitemap']
+
+SITEMAP = {'format': 'xml'}
+
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.with_', 'jinja2.ext.do']}
+
+############################
+#  Content
+############################
+PATH = 'content'            # Content root directory
+ARTICLE_PATHS = ['posts']   # Article directories
+PAGE_PATHS = ['pages']      # Page directories
+IGNORE_FILES = ['drafts']   # Ignored files
+
+# Directories to be copied to Output (paths relative to Content)
+STATIC_PATHS = ['images', 'custom_css', 'extra/CNAME', 'extra/robots.txt']
+# Paths rel to Content : {'path' : Path in Output}
+EXTRA_PATH_METADATA = {
+     'custom_css/castoris.css': {'path': 'castoris.css'},
+     'extra/CNAME': {'path': 'CNAME'},
+     'extra/robots.txt': {'path': 'robots.txt'},
+}
+
+DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
+
+############################
+# Output
+############################
+OUTPUT_PATH = 'output/'     # Output root directory
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'  # article URL
+PAGE_URL = 'pages/{slug}'                            # page URL
+ARTICLE_LANG_URL = 'posts/{slug}-{lang}'             # article lang URL
+CATEGORY_URL = 'category/{slug}/'                    # category index URL
+TAG_URL = 'tags/{slug}/'                             # tag index URL
+
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'         # article filename
+PAGE_SAVE_AS = PAGE_URL + '.html'                    # page filename
+ARTICLE_LANG_SAVE_AS = ARTICLE_URL + '-{lang}.html'  # article lang filename
+CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'       # category index filename
+TAG_SAVE_AS = TAG_URL + 'index.html'                 # tag index filename
+
+
+# Add the field Sortorder to the page MD documentm e.g. Sortorder: 2
+PAGES_SORT_ATTRIBUTE = 'sortorder' # Sort pages by a 'sortorder' attribute
+SUMMARY_MAX_LENGTH = 50            # Summary for Index (words)
+DEFAULT_PAGINATION = 6             # Number of articles per page
+NEWEST_FIRST_ARCHIVES = True       # Order of Articles
+
+############################
+# Feed
+############################
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
