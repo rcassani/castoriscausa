a91c3be352c0fddadce76946e04652db7c71c9f1
Raymundo Cassani <raymundo.cassani@gmail.com>
2020-01-21 18:54:26 -0500

    adds bash script to create the template for a new post

---
+++ new_post.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ $# != 1 ]]; then echo "$0 <article-slug>"; exit 1; fi
+
+cd ./content/drafts/
+
+article_slug=$1
+article_file=${article_slug}.md
+# replace - with _ for the filename
+article_file=$(echo ${article_file} | tr - _)
+touch ${article_file}
+
+echo "Title: " > ${article_file}
+echo "Date: $(date +"%Y-%m-%d %H:%M")" >> ${article_file}
+echo "Category:" >> ${article_file}
+echo "Tags:" >> ${article_file}
+echo "Author: Raymundo Cassani" >> ${article_file}
+echo "Slug: ${article_slug}" >> ${article_file}
+echo "Created content/drafts/${article_file}"
