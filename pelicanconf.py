0b6ff7f91a2f33208c13ce79f2e6449c8c407316
Raymundo Cassani <raymundo.cassani@gmail.com>
2020-03-22 14:32:09 -0400

    Added TOC Markdown extension to have id in headers

--- pelicanconf.py
+++ pelicanconf.py
@@ -107,6 +107,7 @@ MARKDOWN = {
         'markdown.extensions.codehilite': {'css_class': 'highlight'},
         'markdown.extensions.extra': {},
         'markdown.extensions.meta': {},
+        'markdown.extensions.toc': {},
     },
     'output_format': 'html5',
 }
